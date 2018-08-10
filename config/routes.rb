8313079859829d9bb00fe2540c248630f9d0cb3c
eugenius123 <yugene123@gmail.com>
2018-08-10 22:13:47 +0000

    section modal added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :dashboard, only: [:show]
+
   root 'static_pages#index'
+
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
@@ -15,7 +18,7 @@ Rails.application.routes.draw do
     end
 
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+      resources :sections, only: [:create]
     end
   end
 end
